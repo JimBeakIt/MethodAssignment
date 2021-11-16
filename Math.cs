c66807c624ae1d7ae39f550223cc47a57739ae7f
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-11-16 08:37:17 +0000

    Add project files.

---
+++ Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class Operator
+    {
+        public double GetAdd(int data)
+        {
+            data = data + 7;
+            return GetSubtract(data);
+        }
+
+        private double GetSubtract(double data)
+        {
+            data = data - 4;
+            return GetDivide(data);
+        }
+
+        private double GetDivide(double data)
+        {
+            return data / 2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number which I will do some Math operations on.");
+            string line = Console.ReadLine();
+            
+            Data = Int32.Parse(line);
+            
+            var operatorObject = new Operator();
+            var result = operatorObject.GetAdd(Data);
+            
+            Console.WriteLine("Your answer is " + result);
+            Console.ReadLine();
+        }
+    }
+}
